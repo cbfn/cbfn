c06b0b834a03cbb8e5069d04936d97121c098839
Christian Nascimento <cnascimentobr@gmail.com>
2021-03-01 01:21:46 -0300

    change design

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
 html {
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -12,5 +13,22 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+body {
+  color: #121212;
+  background: #8e2de2; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #4a00e0,
+    #8e2de2
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #4a00e0,
+    #8e2de2
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,60 @@
 <template>
-  <div class="container mx-auto px-4 flex flex-col">
-    <h1 class="title"><span>&lt;</span>BFN</h1>
-    <h2>Web, Mobile and Support Development | Company Training</h2>
+  <!-- <div class="container mx-auto px-4 flex flex-col">
+
+    <h2 class="text-white opacity-75">
+      Web, Mobile and Support Development<br />&<br />Company Training
+    </h2>
+  </div> -->
+  <div class="flex flex-row h-screen">
+    <div class="hidden w-1/3 md:flex items-center justify-center h-full">
+      <h1 class="title text-white font-bold text-5xl">
+        <span>&lt;</span>CBFN<span>&gt;</span>
+      </h1>
+    </div>
+    <div
+      class="w-full md:w-2/3 bg-white h-full flex flex-col p-8 justify-center"
+    >
+      <h1 class="block md:hidden title font-bold text-5xl">
+        <span>&lt;</span>CBFN<span>&gt;</span>
+      </h1>
+      <h2 class="text-5xl">Web</h2>
+      <h2 class="text-5xl">Mobile</h2>
+      <h2 class="text-5xl">Support</h2>
+      <h2 class="text-5xl">& Company Training</h2>
+      <div class="space-x-2 text-2xl mt-4">
+        <a
+          href="https://www.instagram.com/cbfn.dev/"
+          title="Follow me on instagram"
+          class="border border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white rounded-full transition-all duration-500 ease-in-out py-4 px-8"
+          >Follow us on <font-awesome-icon :icon="['fab', 'instagram']"
+        /></a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Web, Mobile, Support Development & Company Training | CBFN',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'We create web and mobile applications that helps companies to generate more results for their customers.',
+        },
+      ],
+    }
+  },
+})
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300&display=swap');
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
@@ -29,21 +72,16 @@ export default Vue.extend({})
 
 .title {
   font-family: 'Inter', sans-serif;
-  display: block;
-  @apply font-bold;
-  font-size: 80px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 .title > span {
   @apply font-bold;
-  font-size: 100px;
+  font-size: 60px;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: #f9f9f9;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
